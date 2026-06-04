1b3733631eaf3623480bb9c707e1bda5dfee5873
Servet-Demir <demirservet586@gmail.com>
2026-06-04 17:23:54 +0300

    feat: employees added

---
+++ PTS Projesi/src/data.jsx
@@ -0,0 +1,22 @@
+const employees = [
+    {
+        isim: "Servet",
+        Soyisim: "Demir",
+        Yaş: 19,
+    },
+    {
+        isim: "Arif",
+        Soyisim: "Özcan",
+        Yaş: 24,
+    },
+    {
+        isim: "Ahmet",
+        Soyisim: "Demir",
+        Yaş: 26,
+    },
+    {
+        isim: "Ayşe",
+        Soyisim: "Demir",
+        Yaş: 35,
+    },
+]
